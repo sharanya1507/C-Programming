9b6664b382b3e782e17e931e3d0c690251bb7d19
Sharanya M Kumar <sharanyamohankumar15@gmail.com>
2026-03-08 12:04:47 +0530

    Successfully set up C compiler and Git

---
+++ Variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+    char name[] = "George";
+    int age = 30;
+
+
+    printf("hi am Jon \n" );
+    printf("I am 70 years old \n");
+
+    printf("hi am %s \n", name );
+    printf("I am %d years old \n", age );
+
+    return 0;
+
+}
