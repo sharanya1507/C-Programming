a82fdc196c38dd847624472307d779de7f351a0d
Sharanya M Kumar <sharanyamohankumar15@gmail.com>
2026-03-10 12:41:13 +0530

    Array

---
+++ Array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int array[]= {10,20,30,40,50};
+    char name[] = {"Array"};
+
+    printf("%c\n", name[4]);
+
+    printf("the array contains %d", array[4]);
+}
