27ba1b4a1e1f209b8db2615cea112561a4734c4e
Sharanya M Kumar <sharanyamohankumar15@gmail.com>
2026-03-17 11:49:48 +0530

    while & do-while

---
+++ whileLoop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+int main(){
+
+    int index = 1;
+    while (index <=8)
+    {
+        printf("%d\n", index);
+        index++;
+    }
+
+    int i = 1;
+    
+    do{
+        printf("%d\n", i);
+        i++;
+    }while (i <=5);
+    
+}
