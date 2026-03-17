121f598df62c5fbb87152f7f3d7863b85cd9a3e2
Sharanya M Kumar <sharanyamohankumar15@gmail.com>
2026-03-17 13:00:32 +0530

    for loop(easy peasy)

---
+++ forloop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+int main(){
+
+    int luckynumber[] = {10,4,20,40,50};
+    int i = 0;
+    for(i = 0; i<5;i++){
+        printf("%d\n", luckynumber[i]);
+    }
+}
